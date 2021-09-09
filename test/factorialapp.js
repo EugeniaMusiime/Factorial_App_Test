794e6e9c3591161366e47be7dcad2441a1cc57b3
EugeniaMaria <e.musiime@student.ciu.ac.ug>
2021-09-09 11:43:53 +0300

    Factorialize a number with a FOR loop

--- test/factorialapp.js
+++ test/factorialapp.js
@@ -12,16 +12,12 @@ const assert = chai.assert;
 
 describe('Factorial', function() {
 
-    it('factorial should return correct value', function() {
-        const mathsService = new Factorial();
-        assert.equal(mathsService.factorial(-99),-1);
-
-    });
-    it('factorial should return correct value', function() {
+    /*it('factorial should return correct value', function() {
         const mathsService = new Factorial();
         assert.equal(mathsService.factorial(1),1);
         assert.equal(mathsService.factorial(0),1);
-    });
+    });*/
+    
     it('factorial should return correct value', function() {
         const mathsService = new Factorial();
         assert.equal(mathsService.factorial(4),24);
