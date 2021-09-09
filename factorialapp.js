28d00882446fdc2f17095925286f0822edce0498
EugeniaMaria <e.musiime@student.ciu.ac.ug>
2021-09-09 11:36:58 +0300

    Factorialize a number with a FOR loop

--- factorialapp.js
+++ factorialapp.js
@@ -1,12 +1,42 @@
-module.exports = class maths {
+/*
+GROUP MEMBERS
+1. Eugenia Musiimemaria
+2. Collines Mukasa
+3. Wamanga Peter 
+
+*/
+
+//Factorialize a number with recursion.
+
+/*module.exports = class Factorial {
     factorial(num) {
+        // If the number is less than 0, reject it.
         if (num < 0) 
             return -1
+         // If the number is 0, its factorial is 1.
         else if (num == 0) 
             return 1;
+        // Otherwise, call the recursive procedure again
         else {
             return (num * this.factorial(num - 1));
         }
     }
-};
+};*/
 
+//Factorialize a number with a FOR loop.
+
+module.exports = class Factorial{
+    factorial(num){
+        // If num = 0 OR num = 1, the factorial will return 1
+        if (num === 0 || num === 1)
+            return 1
+
+        // We start the FOR loop with i = 4
+        // We decrement i after each iteration
+        for (var i = num - 1; i >= 1; i--) {
+            // We store the value of num at each iteration
+            num = num * i; 
+          }
+        return num;
+    }
+}
--- test/factorialapp.js
+++ test/factorialapp.js
@@ -6,25 +6,24 @@ GROUP MEMBERS
 3. Wamanga Peter 
 
 */
-const MathsService = require('../factorialapp');
+const Factorial = require('../factorialapp');
 const chai = require('chai');
-const expect = chai.expect;
 const assert = chai.assert;  
 
 describe('Factorial', function() {
 
     it('factorial should return correct value', function() {
-        const mathsService = new MathsService();
+        const mathsService = new Factorial();
         assert.equal(mathsService.factorial(-99),-1);
 
     });
     it('factorial should return correct value', function() {
-        const mathsService = new MathsService();
+        const mathsService = new Factorial();
         assert.equal(mathsService.factorial(1),1);
         assert.equal(mathsService.factorial(0),1);
     });
     it('factorial should return correct value', function() {
-        const mathsService = new MathsService();
+        const mathsService = new Factorial();
         assert.equal(mathsService.factorial(4),24);
         assert.equal(mathsService.factorial(6),720);
      
